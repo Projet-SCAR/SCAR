bcd22780149b20300434841d75f67c46bf7e4963
EwanLC <90306617+EwanLC@users.noreply.github.com>
2023-04-20 11:44:14 +0200

    commenté et nettoyé
    x3 nuzzle pounces on you , uwu u so warm.
    couldnt help but notice your ..

--- Transmission/Raspberry PI/Dialogue.py
+++ Transmission/Raspberry PI/Dialogue.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python
-import csv
-import smbus
-import time
-import struct
-import serial
-ser=serial.Serial('/dev/ttyS0',9600)
+import csv   # On importe les bibliothèque dont nous avons besoin ^^
+import smbus   # On importe les bibliothèque dont nous avons besoin ^^
+import time   # On importe les bibliothèque dont nous avons besoin ^^
+import struct   # On importe les bibliothèque dont nous avons besoin ^^
+import serial   # On importe les bibliothèque dont nous avons besoin ^^
+ser=serial.Serial('/dev/ttyS0',9600) # On configure la liaison serie >< *crazy*
 ser.timeout = 5
 bus = smbus.SMBus(1)
 time.sleep(1)
 address = 0x8
 d = open('data.csv','w',newline='')
 w = csv.writer(d)
-while True:
-    try:
+while True:  #Boucle infinie (true = vrai, while = tant que -> tant que vrai -> tout le temps quoi :p)
+    try: #S'il n'y a pas de problème dans la liaison
         L = bus.read_i2c_block_data(address,0,12)
+        #Création des listes qui vont recevoir les données
         templ = []
         huml = []
         luml = []
+        #byte --> float pour les 3 valeurs
         for i in range(0, 4):
             templ.append(L[i])
         tempb = bytearray(templ)
         temp = struct.unpack('<f',tempb[0:4])
-        temp = temp[0]
-        for i in range(4,8):
+        temp = temp[0] 
+        for i in range(4,8): 
             huml.append(L[i])
         humb = bytearray(huml)
         hum = struct.unpack('<f',humb[0:4])
@@ -32,19 +34,18 @@ while True:
         lumb = bytearray(luml)
         lum = struct.unpack('<f',lumb[0:4])
         lum = lum[0]
+        # Lister et arrondir /shrug, aussi on date les valeurs
         Li = [round(temp,1),round(hum,1),round(lum),time.ctime()]
     except:
-#         import pdb
-#         pdb.set-trace()
+        #S'il il n'il y arrive pas on enregistre sans valeur (NaN = Not a Number)
         Li=['NaN','NaN','NaN',time.ctime()]
     try:
-        #ser.write(tempb+humb+lumb) bytes(str(Li),"utf-8")
+        #transfer de la liste e valeurs par module LoRa
         ser.write(bytes(str(Li)+"\n","utf-8"))
     except:
+        #lire plus haut
         ser.write(b'NaN')
-    print([tempb,humb,lumb])
-    print(tempb+humb+lumb) 
-    print(Li)
+    #ecriture csv
     w.writerow(Li)
     d.flush()
     time.sleep(1);
