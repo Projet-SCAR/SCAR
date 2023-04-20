e1972ee61f4a0a370d4e932cf2ee6bdc4c681535
EwanLC <90306617+EwanLC@users.noreply.github.com>
2023-04-20 11:50:02 +0200

    Rien changé
    Juste pour dire que c'est le code de l'arduino et qu'il va pas ici

--- Transmission/Raspberry PI/Programme.ino
+++ Transmission/Raspberry PI/Programme.ino
@@ -3,7 +3,7 @@
 // Written by Tony DiCola for Adafruit Industries
 // Released under an MIT license.
 
-// REQUIRES the following Arduino libraries:
+// REQUIRES the following Arduino libraries: 
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 // - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
